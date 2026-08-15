d63b1e5ec5c8767e2e209adcc795382328a9f714
Huthaifa Hyasat <huthaifa.hyasat@ai-makers.com>
2026-08-15 04:33:03 +0300

    Part 4 - Added Enrollment grade encapsulation

--- main.py
+++ main.py
@@ -26,19 +26,35 @@ class Course:
 
     def __str__(self):
         return f"Course: {self.code} - {self.name} - Seats: {self.seats}"
+class Enrollment:
+    def __init__(self, student, course, grade):
+        self.student = student
+        self.course = course
+        self.grade = grade
 
+    @property
+    def grade(self):
+        return self.__grade
+
+    @grade.setter
+    def grade(self, value):
+        if value < 0 or value > 100:
+            raise ValueError("Grade must be between 0 and 100.")
+        self.__grade = value
 
 person = Person(100, "Ahmad")
 student = Student(101, "Lina", "Computer Science")
 
 print(person.describe())
 print(student.describe())
-
-
 print(student)
 
 course1 = Course("CS101", "Introduction to Programming", 2)
 course2 = Course("DB101", "Introduction to Databases", 3)
-
 print(course1)
 print(course2)
+
+
+enrollment = Enrollment(student, course1, 85)
+print(enrollment.grade)
+enrollment.grade = 150
